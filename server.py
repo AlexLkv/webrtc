c9ab34aa5ea1588d2fa6c15cf37cfe82d87b4cb8
AlexLkv <83674613+AlexLkv@users.noreply.github.com>
2025-04-02 12:02:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -107,4 +107,4 @@ def on_data(data):
     socketio.emit('data', data, room=target_sid)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0', port=8080)
